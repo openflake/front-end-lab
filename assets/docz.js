d676b8ed84fe686cd35a55728691e787852b3d99
seatwork <mophisoft@gmail.com>
2020-11-08 21:08:00 +0800

    create unique loader

--- assets/docz.js
+++ assets/docz.js
@@ -4,34 +4,18 @@ document.addEventListener('DOMContentLoaded', function(e) {
     const menu = document.querySelector('menu')
     const main = document.querySelector('main')
 
-    function toggleSidebar() {
-        if (sidebar.isOpen) {
-            sidebar.isOpen = false
-            sidebar.style.left = '-300px'
-        } else {
-            sidebar.isOpen = true
-            sidebar.style.left = 0
-        }
-    }
+    const loader = new Image()
+    loader.className = 'loading'
+    loader.src = '/assets/loader.svg'
 
-    function createFrame() {
-        let iframe = document.querySelector('iframe')
-        if (!iframe) {
-            iframe = document.createElement('iframe')
-            main.classList.add('with-frame')
-            main.appendChild(iframe)
-        }
-        return iframe
-    }
-
-    function createLoader() {
-        const loader = new Image()
-        loader.className = 'loading'
-        loader.src = '/assets/loader.svg'
-        main.innerHTML = ''
-        main.appendChild(loader)
-        return loader
-    }
+    menu.addEventListener('click', toggleSidebar)
+    fetch('/README.md').then(res => res.text()).then(res => {
+        main.innerHTML = marked(res)
+    })
+    fetch('/SIDEBAR.md').then(res => res.text()).then(res => {
+        nav.innerHTML = marked(res)
+        addLinksClick()
+    })
 
     function addLinksClick() {
         const links = sidebar.querySelectorAll('a')
@@ -39,12 +23,13 @@ document.addEventListener('DOMContentLoaded', function(e) {
             link.addEventListener('click', function(e) {
                 e.preventDefault()
                 toggleSidebar()
+                main.innerHTML = ''
+                main.appendChild(loader)
 
                 let url = this.getAttribute('href')
                 if (url.endsWith('/')) {
                     url += 'index.html'
                 }
-                const loader = createLoader()
                 if (url.endsWith('.md')) {
                     fetch(url).then(res => res.text()).then(res => {
                         main.innerHTML = marked(res)
@@ -59,12 +44,23 @@ document.addEventListener('DOMContentLoaded', function(e) {
         })
     }
 
-    menu.addEventListener('click', toggleSidebar)
-    fetch('/README.md').then(res => res.text()).then(res => {
-        main.innerHTML = marked(res)
-    })
-    fetch('/SIDEBAR.md').then(res => res.text()).then(res => {
-        nav.innerHTML = marked(res)
-        addLinksClick()
-    })
+    function createFrame() {
+        let iframe = document.querySelector('iframe')
+        if (!iframe) {
+            iframe = document.createElement('iframe')
+            main.classList.add('with-frame')
+            main.appendChild(iframe)
+        }
+        return iframe
+    }
+
+    function toggleSidebar() {
+        if (sidebar.isOpen) {
+            sidebar.isOpen = false
+            sidebar.style.left = '-300px'
+        } else {
+            sidebar.isOpen = true
+            sidebar.style.left = 0
+        }
+    }
 })
